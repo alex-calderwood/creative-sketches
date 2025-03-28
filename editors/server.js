b8c53a0b5744d999483d89d16df85c40b0b18a15
Alex Calderwood <alexander.d.calderwood@gmail.com>
2025-03-28 15:14:23 -0700

    update port

--- editors/server.js
+++ editors/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3008;
 
 // Load projects from config file
 let projects = [];
