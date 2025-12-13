cf854470c5c00255844f7cfb905bcc5268a31f01
Alex Calderwood <alexander.d.calderwood@gmail.com>
2025-12-13 13:18:20 -0800

    hyper almost done

---
+++ editors/editors/vert/game.js
@@ -0,0 +1,18 @@
+import { HyperPerformance } from './src/performances/hyper/HyperPerformance.js';
+
+export class Game {
+  constructor(options = {}) {
+    this.performance = null;
+    this.tickInterval = 2000; // ms between ticks
+  }
+
+  async initialize(options = {}) {
+    this.performance = new HyperPerformance();
+    this.performance.initialize();
+
+    // Start tick loop
+    setInterval(() => {
+      this.performance.tick();
+    }, this.tickInterval);
+  }
+}
