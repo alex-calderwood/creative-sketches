2b2ccc0f990522cf53bc877c768c402b964255ca
Alex Calderwood <alexander.d.calderwood@gmail.com>
2026-01-07 12:53:13 -0800

    Added pencil

--- editors/editors/pencil-4-delete/editor.js
+++ editors/editors/pencil-4-delete/editor.js
@@ -135,37 +135,6 @@ class Editor {
 }
 
 
-/**
- * Unused
- * 
- * Extracts the text content from a contenteditable element, preserving explicit line breaks.
- *
- * It needed to be this complex for when we were doing rich text, but now the <br> and <div> stuff isn't used.
- * 
- * This function clones the provided element to avoid altering the original content. It then
- * replaces <br> tags and the beginnings of <div> tags with newline characters to preserve
- * the visual representation of line breaks. The function does not modify <span> tags, as they
- * are not typically associated with line breaks. The modified content is then returned as a
- * single string with preserved line breaks.
- *
- * @param {HTMLElement} element - The contenteditable element from which to extract text.
- * @returns {string} The text content of the element with \n characters in place of <br> and <div> tags.
-*/
-function getTextWithWhitespace(element) {
-  let clone = element.cloneNode(true);
-
-  // Replace <br> tags with \n
-  clone.querySelectorAll('br').forEach(br => br.replaceWith('\n'));
-
-  // Replace block elements like <div> with \n and maintain their content
-  clone.querySelectorAll('div').forEach(div => {
-    div.replaceWith('\n', ...div.childNodes);
-  });
-
-  // Extract the textContent from the cloned element
-  return clone.textContent;
-}
-
 
 const vis = new Editor({
   // squiggleColor: 'red'
